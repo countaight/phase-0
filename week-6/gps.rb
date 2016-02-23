ff308797b8dda899c63044e18d619d21b601d699
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-02-22 22:37:42 -0600

    Finish challenge and add reflection

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,8 +1,8 @@
 # Your Names
-# 1)
-# 2)
+# 1) Oscar Delgadillo
+# 2) Charlie Lee
 
-# We spent [#] hours on this challenge.
+# We spent [1] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
@@ -31,6 +31,32 @@ def serving_size_calc(item_to_make, num_of_ingredients)
   end
 end
 
+# Refactored Code
+
+def serving_size_calc(item_to_make, num_of_ingredients)
+  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
+
+  raise ArgumentError.new("#{item_to_make} is not a valid input") unless library.has_key?(item_to_make)
+
+  serving_size = library[item_to_make]
+  remaining_ingredients = num_of_ingredients % serving_size
+
+  if num_of_ingredients >= serving_size
+    print "You can make #{ num_of_ingredients/serving_size } of #{ item_to_make }. "
+  else
+    print "You do not have enough ingredients to make #{ item_to_make }. "
+  end
+  
+  case remaining_ingredients
+    when 6 then print "You can make one cake and one cookie."
+    when 5 then print "You can make one cake."
+    when 1,2,3,4 then print "You can make #{ remaining_ingredients } cookies."
+  end
+  
+  puts
+  
+end 
+
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
@@ -41,3 +67,21 @@ p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
 
+# What did you learn about making code readable by working on this challenge?
+
+# I learned that it's very important. The first error counter was utterly complicated and unnecessary. It's also important to make your code readable to make updating the code easier. As it stood our code isn't very scalable, but at least you can read the parts that still need to be updated.
+
+
+# Did you learn any new methods? What did you learn about them?
+
+# We used hash#has_key? in order to determine the existence of the item in the library and if it wasn't there, an ArgumentError would be raised.
+
+
+# What did you learn about accessing data in hashes? 
+
+# I learned that it's actually very simple to access data, not only through methods, but also through calling the key.
+
+
+# What concepts were solidified when working through this challenge?
+
+# I really appreciated seeing the case flow in action and learning when it's appropriate to use as opposed to the if conditional. I also learned when it was appropriate to have one line conditional statements.
