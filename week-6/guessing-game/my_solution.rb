27b708644ea1c77febd0f96f1acabd5f66f1157d
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-02-28 19:17:10 -0600

    Add reflection

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -102,3 +102,18 @@ p "You solved the game, yay!"
 
 
 # Reflection
+
+# How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+# Instance variables are similar to characteristics in objects, in that they hold values that represent things like names, size, colors, or anything that is unique of that particular object. Methods on the other hand help the object respond to different calls in specific ways, and in that sense is defining the bahavior of that object.
+
+
+# When should you use instance variables? What do they do for you?
+# Instance variables should be used when a particular object needs to hold a specific characteristic or state. For example, an object representing a ticket, might need a venue name, price, and event name. Instance variables also have the advantage of having a larger scope within the class. They are able to be used in any methods created inside the class, and therefore can be altered, read, printed, etc. whenever necessary. So for example, a method that prints the ticket information in a specially formatted way, will have access to those instance variables, venue, price, and event, and be able to display them on the console.
+
+
+# Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+# Flow control is used when multiple inputs are possible and the outcome needs to be different. So for example asking for a guess to the game's answer, a value needs to be outputted dipending on the relationship between the guess and the answer, high, low, or correct. One mini struggle, was trying to make sure the instance variable that held the last result could go from false to true if the correct answer was given, and then an incorrect answer.
+
+
+# Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+# Symbols are similar to strings except when created symbols are unique objects throughout the code, in contrast, every time a string is created, even if it's the same string with the same content, a new object is created for that string. Using symbols for repeated use of the values such as high, low or correct, help cut down on memory usage, since the string isn't created multiple times with multiple object ids.
