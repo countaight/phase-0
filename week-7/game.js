f6a5c5f3a50afbcc5e89464124fe234fd5bb9ffe
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-03-03 14:02:37 -0600

    Work on refactored code

--- week-7/game.js
+++ week-7/game.js
@@ -33,6 +33,94 @@
 
 // Initial Code
 
+// var day = 0;
+// var marketPrice = Math.floor((Math.random() * 2) + 1);
+// console.log("---Welcome to Lemonade Tycoon---", "\nDay", day + 1 )
+// console.log("The market price for lemonade today is $"+marketPrice+".00.");
+// var store = {
+// 	inventory: {
+// 		lemons: 0.3,
+// 		sugar: 3
+// 	}
+// };
+// var kid = {
+// 	wallet: 5,
+// 	inventory: {
+// 		lemons: 0,
+// 		sugar: 0,
+// 		juicer: false //this is for future implementations, a juicer would double your output of lemonade
+// 	},
+// 	location: "home",
+// 	pitchers: 0,
+// 	goToLocation: function(location) {
+// 		if(location) {
+// 			kid.location = location;
+// 			console.log("You are now at", location);
+// 		} else {
+// 			kid.location = "home";
+// 			console.log("You seem to have wandered about until you found your home!");
+// 		}
+// 	},
+// 	buy: function(item, amount) {
+// 		if(kid.location === "store") {
+// 			switch(item) {
+// 				case "lemons":
+// 					if(kid.wallet >= (3 * (amount * 10)/100)) {
+// 						kid.wallet = ((kid.wallet * 10) - (store.inventory[item] * 10) * amount)/ 10;
+// 						kid.inventory[item] += amount;
+// 					} else {
+// 						console.log("You don't have enough for lemons.");
+// 					}
+// 					break;
+// 				case "sugar":
+// 					if(kid.wallet >= (3 * amount)) {
+// 						kid.wallet = ((kid.wallet * 10) - (store.inventory[item] * 10) * amount)/ 10;
+// 						kid.inventory[item] += amount;
+// 					} else {
+// 						console.log("You don't have enough for sugar.");
+// 					}
+// 					break;
+// 				default:
+// 					console.log("I'm sorry, we don't have that item.");
+// 					break;
+// 			}
+// 		} else {
+// 			console.log("It appears you're not at the store...");
+// 		}
+// 	},
+// 	makeLemonade: function() {
+// 		if(kid.location === "home") {
+// 			while(kid.inventory.lemons >= 2 && kid.inventory.sugar >= 1) {
+// 				kid.pitchers += 1;
+// 				kid.inventory.lemons -= 2;
+// 				kid.inventory.sugar -= 1;
+// 			}
+// 		} else {
+// 			console.log("It appears you're not at home...");
+// 		}
+// 	},
+// 	endDay: function() {
+// 		if(kid.location === "home") {
+// 			kid.wallet += kid.pitchers * 5 * marketPrice;
+// 			kid.pitchers = 0
+// 			console.log("You now have $"+kid.wallet);
+// 			console.log("--------------------------");
+// 			day += 1;
+// 			marketPrice = Math.floor((Math.random() * 2) + 1);
+// 			console.log("Day", day + 1, "\nThe market price for lemonade is $"+marketPrice+".00.")
+			
+// 		} else {
+// 			console.log("You need to go home kid...");
+// 		}
+// 	}
+	
+// }
+
+
+
+
+// Refactored Code
+
 var day = 0;
 var marketPrice = Math.floor((Math.random() * 2) + 1);
 console.log("---Welcome to Lemonade Tycoon---", "\nDay", day + 1 )
@@ -40,9 +128,17 @@ console.log("The market price for lemonade today is $"+marketPrice+".00.");
 var store = {
 	inventory: {
 		lemons: 0.3,
-		sugar: 3
+		sugar: 3,
+		juicer: 100
+	},
+	show: function() {
+		var stock = keys(store.inventory);
+		for(var i = 0; i < stock.length; i++) {
+			console.log(stock[i], "--", store.inventory[stock[i]].toFixed(2));
+		}
 	}
 };
+
 var kid = {
 	wallet: 5,
 	inventory: {
@@ -80,6 +176,15 @@ var kid = {
 						console.log("You don't have enough for sugar.");
 					}
 					break;
+				case "juicer":
+					if(kid.wallet >= 100) {
+						kid.wallet -= 100;
+						kid.inventory[item] = true;
+						delete store.inventory.juicer;
+					} else {
+						console.log("You don't have enough for a juicer.");
+					}
+					break;
 				default:
 					console.log("I'm sorry, we don't have that item.");
 					break;
@@ -91,7 +196,11 @@ var kid = {
 	makeLemonade: function() {
 		if(kid.location === "home") {
 			while(kid.inventory.lemons >= 2 && kid.inventory.sugar >= 1) {
-				kid.pitchers += 1;
+				if(kid.inventory.juicer === false) { 
+					kid.pitchers += 1;
+				} else {
+					kid.pitchers += 2;
+				};
 				kid.inventory.lemons -= 2;
 				kid.inventory.sugar -= 1;
 			}
@@ -103,7 +212,7 @@ var kid = {
 		if(kid.location === "home") {
 			kid.wallet += kid.pitchers * 5 * marketPrice;
 			kid.pitchers = 0
-			console.log("You now have $"+kid.wallet);
+			console.log("You now have $"+kid.wallet.toFixed(2));
 			console.log("--------------------------");
 			day += 1;
 			marketPrice = Math.floor((Math.random() * 2) + 1);
@@ -113,15 +222,7 @@ var kid = {
 			console.log("You need to go home kid...");
 		}
 	}
-	
 }
-
-
-
-
-// Refactored Code
-
-
 
 
 
@@ -159,18 +260,48 @@ assert(
 )
 
 kid.goToLocation("store")
-
 assert(
 	(kid.location === "store"),
 	"The location should read store.",
 	"3. "
 )
 
-kid.buy("lemons", 3);
+kid.buy("lemons", 4);
 kid.buy("sugar", 1);
-
 assert(
-	(kid.wallet === 1.1),
+	(kid.wallet === .8),
 	"The wallet should now have decreased in money.",
 	"4. "
+)
+
+kid.goToLocation();
+assert(
+	(kid.location === "home"),
+	"Not calling a location sends you back home.",
+	"5. "
+)
+
+kid.makeLemonade();
+assert(
+	(kid.pitchers === 1),
+	"Make lemonade yields one pitcher.",
+	"6. "
+)
+
+oldMarketPrice = marketPrice
+kid.endDay();
+assert(
+	(kid.wallet === .8 + oldMarketPrice * 5),
+	"The wallet should now have decreased in money.",
+	"7. "
+)
+
+kid.inventory.juicer = true;
+kid.inventory.lemons = 6; //six lemons to produce 3 pitchers
+kid.inventory.sugar = 3; //three bags of sugar to produce 3 pitchers
+kid.makeLemonade()
+assert(
+	(kid.pitchers === 6),
+	"The juicer should double the output of pitchers.",
+	"8. "
 )
