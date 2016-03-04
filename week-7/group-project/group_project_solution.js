26ffd7aa6f23d2137cad79bfc7efbe5a5e5f1207
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-03-04 14:50:38 -0600

    Add user stories

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,3 @@
+// As a user I want to use a function "sum" to take all the numbers in a list and add them together.
+// As a user I want to use a function "mean" to get the mathematical average from a list of numbers by adding all the numbers on the list together and dividing by the amount of numbers on that list.
+// As a user I want to use a function "median" to get the middle number from a list of numbers that has been sorted from the smallest number to the biggest number. If there are an even amount of numbers on the list I want to know the average of the two middle numbers.
