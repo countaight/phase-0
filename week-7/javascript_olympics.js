bdbed2e7c87e2243380c6c4831d45a6a51e9f46e
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-03-04 15:41:34 -0600

    Add reflection to 7.6

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -2,7 +2,7 @@
 
 // I paired [with: Tomasz ] on this challenge.
 
-// This challenge took me [#] hours.
+// This challenge took me [1.5] hours.
 
 
 // Warm Up
@@ -85,15 +85,10 @@ console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps
 
 // Reflection
 
-// function commas(number) {
-//   var numberString = String(number);
-//   var digitArray = numberString.split("").reverse();
-//   if (numberString.length <= 3) {
-//     return numberString;
-//   } else {
-//     for (var i = 3; i < digitArray.length; i+=4) {
-//       digitArray.splice(i, 0, ",");
-//     };
-//   }
-//   return digitArray.reverse().join("");
-// }
+
+// What JavaScript knowledge did you solidify in this challenge?
+  // Object contruction was definitely new to me, it reminded me of Ruby classes in a way. It also reinforced my knowledge of loops in JavaScript, and how powerful and flexible they can be. For example, in a for a loop, the three statements in the for loop argument can be manipulated in multiple ways to achieve the loop you want, including running the loop for an amount of cycles determined by external factors, like size of a list, or even size of a list as it changes.
+// What are constructor functions?
+  // Constructor functions are functions that essentially create objects (the ones in hashes) by automating the process. It looks like any regular function, except the arguments can be used to add properties. This is great for when you need a group of objects that share similar traits or characteristics, almost like a Ruby class.
+// How are constructors different from Ruby classes (in your research)?
+  // From my research, I discovered that the biggest difference between Ruby classes and JavaScript constructors, is, while a Ruby class can later create a method for all of its instances, a JavaScript constructor can't. The constructor basically creates the instance once and it's no longer related to the function whence it came. Meanwhile, any instance from a Ruby class will be related to it, and any new methods added to the classes will be readily available to those instance objects.
