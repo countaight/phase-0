ccfcbbd46fcfbcaa313fb4ac02e6cc4da8700818
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-03-06 12:34:13 -0600

    Finish group project

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,3 +1,7 @@
 // As a student I want to "sum" the numbers on a list so that I can add together any amount of numbers together.
 // As a student I want to get the "mean", or mathematical average from a list of numbers.
 // As a student I want to get the "median", or middle number, from a set of numbers. I should be able to get a median no matter what the size of the list is.
+
+
+// Tests 1, 4, 7, 8, and 9 passed. Tests 2, 3, 5, 6 did not pass.
+// I believe the test failed because the functions did not return anything.
