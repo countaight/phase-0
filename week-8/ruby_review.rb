f8c8d43d78c1cb9a93abda55da923e2253262f6c
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-03-11 12:21:48 -0600

    Add reflection 8.6

--- week-8/ruby_review.rb
+++ week-8/ruby_review.rb
@@ -19,8 +19,79 @@
 
 # 3. Initial Solution
 
+# class Car
+#   attr_reader :make, :color, :pizza_compartment
+  
+#   def initialize(make, color)
+#     @make = make
+#     @color = color
+#     @distance_travelled = 0
+#     @speed = 0
+#     @orientation = "north"
+#     @pizza_compartment = []
+#   end
+
+#   def drive(miles, mph=@speed)
+#     @distance_travelled += miles
+#     @speed = mph
+#   end
+  
+#   def accelerate(mph)
+#     @speed += mph
+#   end
+  
+#   def decelerate(mph)
+#     @speed -= mph
+#   end
+  
+#   def stop
+#     @speed = 0
+#   end
+  
+#   def turn(direction)
+#     if direction == "left" || direction == "right"
+#       @direction = direction
+#     else
+#       raise ArgumentError.new("You can't turn that direction, son")
+#     end
+#   end
+  
+#   def school_zone
+#     puts @speed
+#     @speed = 15
+#   end
+  
+#   def log
+#     @distance_travelled
+#   end
+  
+#   def pick_up(pizza)
+#     @pizza_compartment << pizza
+#   end
+
+#   def deliver
+#   	@pizza_compartment.shift
+#   end
+  
+# end
+
+# class Pizza
+#   attr_accessor :hotness, :toppings, :crust
+#   def initialize(toppings, crust)
+#     @toppings = toppings
+#     @crust = crust
+#     @hotness = 100
+#   end
+  
+  
+  
+# end
+
+
+# 4. Refactored Solution
+ 
 class Car
-  attr_reader :make, :color
+  attr_reader :make, :color, :pizza_compartment
   
   def initialize(make, color)
     @make = make
@@ -29,27 +100,9 @@ class Car
     @speed = 0
     @orientation = "north"
     @pizza_compartment = []
-    @pizza_counter = 0
   end
-  
-  def pick_up(pizza)
-    @pizza_compartment << pizza
-  end
-# create counter that takes away pizzas hotness
-# and resets each time
-# if distance to drive is greater than 0.25, divide distance by 0.25 and any remainder goes into counter
-  
-  def counter(miles)
-    while @pizza_counter >= 0.25 do
-      @pizza_compartment.each do |pizza|
-        pizza.hotness -= 1
-      end
-      @pizza_counter -= 0.25
-    end
-  end
-  
+
   def drive(miles, mph=@speed)
-    counter(miles)
     @distance_travelled += miles
     @speed = mph
   end
@@ -67,40 +120,42 @@ class Car
   end
   
   def turn(direction)
-    if direction == "left" || direction == "right"
-      @direction = direction
-    else
-      raise ArgumentError.new("You can't turn that direction, son")
-    end
+    raise ArgumentError.new("You can't turn that direction, son") unless direction == "left" || direction == "right"
+    @direction = direction
   end
   
   def school_zone
-    puts @speed
+    puts "Your current speed is #{@speed}mph."
     @speed = 15
   end
   
-  def status
-    status_array = [@distance_travelled, @speed, @direction]
+  def log
+    @distance_travelled
   end
   
+  def pick_up(pizza)
+    @pizza_compartment << pizza
+  end
+
+  def deliver
+  	@pizza_compartment.shift
+  end
+
 end
 
-
-
-# 4. Refactored Solution
-
 class Pizza
-  attr_accessor :hotness
+  attr_accessor :toppings, :crust
   def initialize(toppings, crust)
     @toppings = toppings
     @crust = crust
-    @hotness = 100
   end
   
   
   
-end
-
+end 
+  
+  
+# end
 
 
 
@@ -118,7 +173,13 @@ funky_car.stop
 funky_car.turn("left")
 funky_car.drive(1.4, 35)
 funky_car.stop
-p funky_car.status
+p "You have travelled #{funky_car.log} miles"
+funky_car.pick_up(Pizza.new("Pepperoni","thin"))
+funky_car.pick_up(Pizza.new("Sausage","thick"))
+p funky_car.pizza_compartment
+funky_car.deliver
+puts "Delivering the pepperoni first in, leaves the sausage pizza"
+puts funky_car.pizza_compartment[0].toppings == "Sausage"
 
 
 
@@ -126,9 +187,20 @@ p funky_car.status
 
 # 5. Reflection
 
+# What concepts did you review or learn in this challenge?
+
+# I learned composition, having one class interact with another without needing to know how the other one does its job. By having classes work independently of each other you minimize convoluted errors but also make your code more DRY and readable.
 
 
-#attr_reader for distance travelled if needed!!!
+# What is still confusing to you about Ruby?
+
+# SO far the only thing that is really confusing me is knowing which "syntatic sugar" is more useful, which is more appropriate, and how do I use to to make my code faster. As of yet, I have not idea how to measure the speed of my program, or how I would go about improving performance.
+
+# What are you going to study to get more prepared for Phase 1?
+
+# I am going to continue using The Well-Grounded Rubyist to elaborate on some topics like modules, lamdas, and procs. I also intend on looking up regular expressions and their uses. Finally, I would like to delve deeper into the use of recursion.
+
+
 
 
 
